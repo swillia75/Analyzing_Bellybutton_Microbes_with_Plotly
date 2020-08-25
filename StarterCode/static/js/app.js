2b9df3888c2f332bea40853842147aed4e8b0be3
Shawn Williams <swillia75@gmail.com>
2020-08-25 15:35:38 -0400

    added slice and sort

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,35 +2,42 @@
 d3.json("data/samples.json").then(function(data) {
     console.log(data);
 
-    var names = [];
-    var metadata = [];
-    var samples = [];
+    let names = [];
+    let metadata = [];
+    let samples = [];
 
     Object.entries(data).forEach(([key, value]) => {
-        if (key === "names"){
+        if (key === "names") {
             names.push(value);
         };
-        if (key === "metadata"){
+        if (key === "metadata")  {
             metadata.push(value);
         };
-        if (key === "samples"){
+        if (key === "samples") {
             samples.push(value);
         };
-              
+        
+    }); 
+    console.log(names);
+    console.log(metadata);
+    console.log(samples.id)
+
+ 
+        
+    // var sortedSamples = samples.sort((a, b) => b.sample_values - a.sample_values);
+
+    // console.log(sortedSamples)
+    
+    // var slicedSamples = sample_values.slice(0, 10).reverse();
+    
+    // console.log(slicedSamples);
 });    
 
-console.log(names);
-console.log(metadata);
-console.log(samples);});
 
-var sortedSamples = samples.sort(function compareFunction(a, b){
-    return b.samples_value - a.samples_value
-});
 
-console.log(sortdSamples)
 
-var slicedSamples = sortedSamples.slice(0, 10);
+// var slicedSamples = samples.slice(0, 10);
 
-console.log(slicedSamples);
-// var otuID = sample.otu_ids
-// var otuCounts
+// console.log(slicedSamples);
+// // var otuID = sample.otu_ids
+// // var otuCounts
