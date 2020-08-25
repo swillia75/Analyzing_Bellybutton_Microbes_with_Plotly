9a019a690a6d28c1ad0bb786a79cb4fbebf71ada
Shawn Williams <swillia75@gmail.com>
2020-08-25 11:28:47 -0400

    created data variables

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,35 @@
 d3.json("data/samples.json").then(function(data) {
     console.log(data);
 
-    Object.entries(data).forEach(([key, value]) => 
-        console.log(key, value));
-            
+    var names = [];
+    var metadata = [];
+    var samples = [];
+
+    Object.entries(data).forEach(([key, value]) => {
+        if (key === "names"){
+            names.push(value);
+        };
+        if (key === "metadata"){
+            metadata.push(value);
+        };
+        if (key === "samples"){
+            samples.push(value);
+        };
+              
 });    
+
+console.log(names);
+console.log(metadata);
+console.log(samples);});
+
+var sortedSamples = samples.sort(function compareFunction(a, b){
+    return b.samples_value - a.samples_value
+});
+
+console.log(sortdSamples)
+
+var slicedSamples = sortedSamples.slice(0, 10);
+
+console.log(slicedSamples);
+// var otuID = sample.otu_ids
+// var otuCounts
