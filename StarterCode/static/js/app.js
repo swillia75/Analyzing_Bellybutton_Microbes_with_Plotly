bd82679caac3ddf51708cb4b07e44428478fa80a
Shawn Williams <swillia75@gmail.com>
2020-08-31 15:49:44 -0400

    more additions

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -46,12 +46,14 @@ function init(){
     var otuCount = firstDataset.map(function(samples) {
         return samples.sample_values;
     }); 
+
+    console.log(otuCount);
     
     //Slicing the first 10 sample_vaues
-    var colonies = otuCount.slice(0, 10);
+    var colonies = otuCount.slice(0, 10, ",");
     console.log(colonies);
    
-    // console.log(colonies)
+    console.log(colonies);
        
     //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
@@ -60,33 +62,37 @@ function init(){
       return samples.otu_labels;
     });
 
-    var labels= otuLabels.slice(0,10);
-    console.log(labels)
+  
+    console.log(otuLabel);
+
+    var labels= otuLabel.slice(0, 10, ",");
+    console.log(labels);
     //Create trace for horizontal bar chart
-        
+  // });      
     var tracehbar = {
       x: colonies,
       y: bbmicrobes,
-      hovertext: labels
       type: "bar",
+      hovertext: labels,
+      
       name: "BellyBotton Biodiversity",
       orientation: "h"
     };
             
-    // Create the data array for the plot
+//     Create the data array for the plot
     var data = [tracehbar];
             
-    // Define the plot layout
+//     Define the plot layout
     var layout = {
         title: "BellyBotton Biodiversity",
         xaxis: { title: "Count" },
         yaxis: { title: "Otu ID" }
     };
             
-    // Plot the chart to a div tag with id "bar"
+//     Plot the chart to a div tag with id "bar"
     Plotly.newPlot("bar", data, layout);
 
-    //Create function to filter first subject metadata for panel    
+//     Create function to filter first subject metadata for panel    
     function initialMetadata(data) {
       return data.id === 940;
     };
@@ -94,18 +100,18 @@ function init(){
     var firstMetadata = metadata.filter(initialMetadata);
     console.log(firstMetadata)
 
-    //Loop through firstMetadata to use object.entries to append key,value pairs to sample_metadata
+  //  Loop through firstMetadata to use object.entries to append key,value pairs to sample_metadata
     firstMetadata.forEach((data) => {
         Object.entries(data).forEach(([key, value]) => {
-          d3.select("ul").append("li").text(`${key} ${value}`);
+          d3.select("ul").append("li").text(`${key}  :  ${value}`);
         });
     });
-
+  
            
-    //Create a bubble chart that displays each sample.
+//     Create a bubble chart that displays each sample.
    
     
-    //Create trace for bubble chart
+//     Create trace for bubble chart
     var tracebubble = {
       x: microbes,
       y: otuCount,
@@ -118,33 +124,33 @@ function init(){
       }
     };
     
-    //Create data array for bubble plot
+//     Create data array for bubble plot
     var bubbledata = [tracebubble];
     
-    //Create layout for bubble plot
-    var layout = {
-      title: 'Marker Size',
+//     Create layout for bubble plot
+    var bubblelayout = {
+      title: 'BellyBottom Microbe Diversity',
       showlegend: false,
-      height: 600,
-      width: 600
+      height: 800,
+      width: 1800
     };
     
-    //Plot the chart to a div tag with id "bubble"
-    Plotly.newPlot('bubble', bubbledata, layout);
+//     Plot the chart to a div tag with id "bubble"
+    Plotly.newPlot('bubble', bubbledata, bubblelayout);
   
-    //Gauge chart
+//     Gauge chart
 
-    //Create array for Wfreq and other
+//     Create array for Wfreq and other
     var wfreq = []
     var other = []
 
-    //Loop through first metadata for wfreq
+//     Loop through first metadata for wfreq
     firstMetadata.forEach((data) => {
 
-      // Iterate through each key and value
-      Object.entries(recipe).forEach(([key, value]) => {
+//       Iterate through each key and value
+      Object.entries(data).forEach(([key, value]) => {
     
-        // Use the key to determine which array to push the value to
+//         Use the key to determine which array to push the value to
         if (key === "wfreq") {
           wfreq.push(value);
         }
@@ -152,8 +158,10 @@ function init(){
           other.push(value);
         };
       });
+    });
 
-    //Use wfreq array to generate gauge plot
+    console.log(wfreq);
+//     Use wfreq array to generate gauge plot
 
     var traceGauge = {
       type: 'pie',
@@ -166,26 +174,26 @@ function init(){
       textinfo: 'text',
       textposition: 'inside',
       marker: {
-        colors: ['','','','','','','','','','white'],
+        colors: ['red','orange','yellow','tan','green','blue','purple','maroon','grey', 'white'],
         labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
         hoverinfo: 'label'
       }
     };
 
  
-
-    var degrees = 50, radius = .9
+    var degrees = 20
+    var radius = 0.9
     var radians = degrees * Math.PI / 180
-    var x = -1 * radius * Math.cos(radians) * wfreq
+    var x = 1 * radius * Math.cos(radians) * 7
     var y = radius * Math.sin(radians)
     
     var gaugeLayout = {
       shapes: [{
         type: 'line',
-        x0: 0,
-        y0: 0,
-        x1: 0.6,
-        y1: 0.6,
+        x0: 0.5,
+        y0: 0.5,
+        x: x,
+        y: y,
         line: {
           color: 'black',
           width: 3
@@ -199,27 +207,27 @@ function init(){
     var dataGauge = [traceGauge]
     
     Plotly.plot('gauge', dataGauge, gaugeLayout)
-  
   });
-};
 
-//On click dropdown menu call function optionChanged
+};
+// On click dropdown menu call function optionChanged
 d3.selectAll("#selDataset").on("change", optionChanged);
 
-//Create function to be called by dropdown click
+// Create function to be called by dropdown click
 function optionChanged() {
 
   var dropdownMenu = d3.selectAll("#selDataset").node();
 
-  //Assign value of the dropdown option to a variable
+//   Assign value of the dropdown option to a variable
   var option = dropdownMenu.value;
+
   console.log(option)
 
-  //Read data from json file
+//   Read data from json file
   d3.json("data/samples.json").then(function(data) {
     console.log(data);
 
-    //create arrays from json object
+//     create arrays from json object
     var names = Object.values(data.names);
     var metadata = Object.values(data.metadata);
     var samples = Object.values(data.samples);
@@ -228,18 +236,18 @@ function optionChanged() {
     console.log(metadata);
     console.log(samples);
 
-    //Initialize empty arrays for sample_values, otu_ids, and otu_labels
+//     Initialize empty arrays for sample_values, otu_ids, and otu_labels
     var selectValues = [] 
     var selectotuID = []  
     var selectLabels = []  
 
-    //Loop through samples array and look for sample.id === option
+//     Loop through samples array and look for sample.id === option
     for (var j = 0; j < samples.length; j++) {
 
       if (option === samples[j].id) {
 
         // When option === sample.id, sample_values, otu_ids, 
-        //and otu_labels are pushed to the empty arrays
+//         and otu_labels are pushed to the empty arrays
         selectValues.push(samples[j].sample_values);
         selectotuID.push(samples[j].otu_ids);
         selectLabels.push(samples[j].otu_labels)
@@ -248,37 +256,44 @@ function optionChanged() {
       
     };
 
+    console.log(option);
     console.log(selectValues);
     console.log(selectotuID);
     console.log(selectLabels);
 
 
-    //Clear sample-metadata to add current metadata
+//     Clear sample-metadata to add current metadata
     var clearMetadata = d3.select('#sample-metadata');
     
     clearMetadata.html("")
 
-    //Create array for current metadata
+//     Create array for current metadata
     var currentMetadata = []
-
-    currentMetadata.forEach((data) => {
-      Object.entries(data).forEach(([key, value]) => {
-        d3.select("ul").append("li").text(`${key} ${value}`);
-      });
-    });
-
-    //Loop through metadata object to metadata.id === option
+    
+    //     Loop through metadata object to metadata.id === option  
     for (var i = 0; i < metadata.length; j++) {
 
       if (parseInt(option) === metadata[i].id) {
 
-        //Push metadata to currentMetadata
+//         Push metadata to currentMetadata
         currentMetadata.push(metadata[i]);
-        
-        
       };
     };
     console.log(currentMetadata);
+    currentMetadata.forEach((data) => {
+        
+
+
+      Object.entries(data).forEach(([key, value]) => {
+        d3.select("ul").append("li").text(`${key}  :  ${value}`);
+      });
+    });
+
+
+      
+        
+      
+    
 
 
 
@@ -293,41 +308,44 @@ function optionChanged() {
     updatePlotly("bubble", "x", [selectValues]);
     updatePlotly("bubble", "y", [selectotuID]);
     updatePlotly("bubble", "hovertext", [selectLabels]);
+
+
     // var microbes = selectotuID.toString().split(",");
-    // console.log(microbes)
-    // var bbmicrobes = microbes.slice(0, 10);
-    // console.log(bbmicrobes);
+//     console.log(microbes)
+//     var bbmicrobes = microbes.slice(0, 10);
+//     console.log(bbmicrobes);
                
             
             
-    // var otuCount = selectValues.map(function(counts) {
-    //      return counts;
-    // }); 
+//     var otuCount = selectValues.map(function(counts) {
+//          return counts;
+//     }); 
     
-    // // var colonies = otuCount.slice(0, 10);              
-    // // console.log(colonies);
-    // var cFu = otuCount.toString().split(",");
-    // var bubbleColonies = cFu.slice(0, 10, ",");
+//     // var colonies = otuCount.slice(0, 10);              
+//     // console.log(colonies);
+//     var cFu = otuCount.toString().split(",");
+//     var bubbleColonies = cFu.slice(0, 10, ",");
     
  
-    // };
+    
             
     
      
         
     
 
-    // //Gauge chart
+
   });
 };
+
 function updatePlotly(newData) {
+
   Plotly.restyle("bar", "values", [newData]);
   Plotly.restyle("bubble", "values", [newData]);
-  Plotly.restyle("gauge", "values", [newData]);
+  Plotly.restyle("gauge", "values", [newData])
   
   
-}
+};
+
 init();
-
-
 
