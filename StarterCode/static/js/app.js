9dd011d748e1e03c5334d4155a66b3598cf885c0
Shawn Williams <swillia75@gmail.com>
2020-09-02 14:05:59 -0400

    finished initial gauge chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -188,49 +188,49 @@ function init(){
 
 //     Use wfreq array to generate gauge plot
 
-var traceGauge = {
-  type: 'pie',
-  showlegend: false,
-  rotation: 90,
-  values: [ 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180],
-  text: ['0','1','2','3','4','5','6','7','8','9'],
-  direction: 'clockwise',
-  textinfo: 'text',
-  textposition: 'inside',
-  marker: {
-    colors: ['red','orange','yellow','tan','green','blue','purple','maroon','grey', 'cyan', 'white'],
-    labels: ['0','1','2','3','4','5','6','7','8','9'],
-    
-  }
-};
+    var traceGauge = {
+      type: 'pie',
+      rotation: 90,
+      values: [ 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180],
+      text: ['0','1','2','3','4','5','6','7','8','9'],
+      direction: 'clockwise',
+      textinfo: 'text',
+      textposition: 'inside',
+      marker: {
+        colors: ['red','orange','yellow','tan','green','blue','purple','maroon','grey', 'cyan', 'white'],
+        labels: ['0','1','2','3','4','5','6','7','8','9'],
+        hoverinfo: 'label'
+      }
+    };
 
- 
-    var degrees = 18
-    var radius = 0.9
-    var radians = degrees * Math.PI / 180
-    var x = 1 * radius * Math.cos(radians) * 7
-    var y = radius * Math.sin(radians)
-    
-    var needleLayout = {
+
+    var degrees = 97, radius = 1.0;
+
+    var radians = degrees * Math.PI / 180;
+    var x = -1 * radius * Math.cos(radians) * wfreq;
+    var y = radius * Math.sin(radians);
+
+    var gaugeLayout = {
       shapes: [{
         type: 'line',
         x0: 0.5,
         y0: 0.5,
-        x1: 0.65,
-        y1: 0.65,
+        x1: x,
+        y1: y,
         line: {
           color: 'black',
           width: 3
         }
       }],
+
       title: 'Belly Button washing Frequency',
       xaxis: {visible: false, range: [-1, 1]},
       yaxis: {visible: false, range: [-1, 1]}
-    }
-    
-    var dataGauge = [traceGauge]
-    
-    Plotly.plot('gauge', dataGauge, needleLayout)
+    };
+
+    var dataGauge = [traceGauge];
+
+    Plotly.newPlot('gauge', dataGauge, gaugeLayout);
   });
 
 };
@@ -248,228 +248,227 @@ function optionChanged() {
   console.log(option)
 
 //Read data from json file
-d3.json("data/samples.json").then(function(data) {
-  console.log(data);
+  d3.json("data/samples.json").then(function(data) {
+    console.log(data);
 
-  //Create arrays from json object
-  var names = Object.values(data.names);
-  var metadata = Object.values(data.metadata);
-  var samples = Object.values(data.samples);
+    //Create arrays from json object
+    var names = Object.values(data.names);
+    var metadata = Object.values(data.metadata);
+    var samples = Object.values(data.samples);
 
-  console.log(names);
-  console.log(metadata);
-  console.log(samples);
+    console.log(names);
+    console.log(metadata);
+    console.log(samples);
 
-  //Loop through names array and add names to dropdown menu
-  names.forEach((name) => {
-    d3.selectAll("select")
-    .append("option").text(name).property("value", name)
-  });
-
-  //FIlter samples and metadata arrays for option
-  function initialSubject(subject) {
-    return subject.id === option;
-  };  
-
-  var firstDataset = samples.filter(initialSubject);
-  console.log(firstDataset)
-
-
-  //creating array of otu_ids for plots
-  var Id = firstDataset.map(function(samples) {
-      return samples.otu_ids;
-  });
-
-  console.log(Id);
-    
-  //Convert Id array to strings and slice the first 10 otu_ids for plots
-  var microbes = Id.toString().split(",");
-  var bbmicrobes = microbes.slice(0, 10, ",");
-  console.log(bbmicrobes);
-             
-          
-  // creating array of sample_values        
-  var otuCount = firstDataset.map(function(samples) {
-      return samples.sample_values;
-  }); 
-
-  console.log(otuCount);
-  
-  //Slicing the first 10 sample_vaues
-  var colonies = otuCount.toString().split(",");
-  var cfu = colonies.slice(0, 10, ",");
-  var count = cfu.toString()
-  console.log(count);
- 
-  var bugs = []
-    
-    for (i=0; i<cfu.length; i++){
-      bugs.push(Number(cfu[i]))
-    }
-
-    console.log(bugs)
-  //Create a horizontal bar chart with a dropdown menu to display the top 
-  //10 OTUs found in that individual.
-
-  //Use otu_labels as the hovertext for the chart.
-  var otuLabel = firstDataset.map(function(samples) {
-    return samples.otu_labels;
-  });
-
-
-  var labels = otuLabel.toString().split(";");
-  var organism = labels.slice(0, 10, ";");
-  console.log(organism);
-
-  //Create trace for horizontal bar chart
-  // });      
-  var tracehbar = {
-    x: bugs,
-    y: bbmicrobes,
-    type: "bar",
-    orientation: "h",
-    hovertext: organism
- 
-  };
-        
-    //     Create the data array for the plot
-  var data = [tracehbar];
-        
-  //     Define the plot layout
-  var layout = {
-    title: "BellyBotton Biodiversity",
-    xaxis: { title: "Count" },
-    yaxis: { title: "Otu ID",
-            autotick: true},
-    
-};
-        
-//     Plot the chart to a div tag with id "bar"
-  Plotly.newPlot("bar", data, layout);
-
-//     Create function to filter first subject metadata for panel    
-  function initialMetadata(data) {
-    return data.id === parseInt(option);
-  };
-
-  var clearMetadata = d3.select('#sample-metadata');
-    
-    clearMetadata.html("")
-
-  var currentMetadata = metadata.filter(initialMetadata);
-  console.log(currentMetadata)
-
-//  Loop through firstMetadata to use object.entries to append key,value pairs to sample_metadata
-  currentMetadata.forEach((data) => {
-      Object.entries(data).forEach(([key, value]) => {
-        d3.select("#sample-metadata").append("ul").append("li").text(`${key}  :  ${value}`);
-      });
-  });
-
-         
-//     Create a bubble chart that displays each sample.
- 
-  
-//     Create trace for bubble chart
-  var tracebubble = {
-    x: microbes,
-    y: colonies,
-    type: "scatter",
-    mode: 'markers',
-    hovertext: labels,
-  
-    marker: {
-      size: colonies,
-      color: microbes,
-      symbol: "circle"
-   
-    },
-  
-  };
-  
-//Create data array for bubble plot
-  var bubbledata = [tracebubble];
-  
-//Create layout for bubble plot
-  var bubblelayout = {
-    title: 'BellyBottom Microbe Diversity',
-    showlegend: false,
-    height: 800,
-    width: 1000
-  };
-  
-//Plot the chart to a div tag with id "bubble"
-  Plotly.newPlot('bubble', bubbledata, bubblelayout);
-
-//     Gauge chart
-
-//Create array for Wfreq and other
-  var wfreq = []
-  var other = []
-
-//Loop through first metadata for wfreq
-  currentMetadata.forEach((data) => {
-
-//Iterate through each key and value
-    Object.entries(data).forEach(([key, value]) => {
-  
-//Use the key to determine which array to push the value to
-      if (key === "wfreq") {
-        wfreq.push(value);
-      }
-      else {
-        other.push(value);
-      };
+    //Loop through names array and add names to dropdown menu
+    names.forEach((name) => {
+      d3.selectAll("select")
+      .append("option").text(name).property("value", name)
     });
-  });
 
-  console.log(wfreq);
-// Use wfreq array to generate gauge plot
+    //FIlter samples and metadata arrays for option
+    function initialSubject(subject) {
+      return subject.id === option;
+    };  
 
-  var traceGauge = {
-    type: 'pie',
-    rotation: 90,
-    values: [ 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180],
-    text: ['0','1','2','3','4','5','6','7','8','9'],
-    direction: 'clockwise',
-    textinfo: 'text',
-    textposition: 'inside',
-    marker: {
-      colors: ['red','orange','yellow','tan','green','blue','purple','maroon','grey', 'cyan', 'white'],
-      labels: ['0','1','2','3','4','5','6','7','8','9'],
-      hoverinfo: 'label'
-    }
-  };
+    var firstDataset = samples.filter(initialSubject);
+    console.log(firstDataset)
 
 
-  var degrees = 18, radius = 0.9;
+    //creating array of otu_ids for plots
+    var Id = firstDataset.map(function(samples) {
+        return samples.otu_ids;
+    });
+
+    console.log(Id);
+      
+    //Convert Id array to strings and slice the first 10 otu_ids for plots
+    var microbes = Id.toString().split(",");
+    var bbmicrobes = microbes.slice(0, 10, ",");
+    console.log(bbmicrobes);
+              
+            
+    // creating array of sample_values        
+    var otuCount = firstDataset.map(function(samples) {
+        return samples.sample_values;
+    }); 
+
+    console.log(otuCount);
+    
+    //Slicing the first 10 sample_vaues
+    var colonies = otuCount.toString().split(",");
+    var cfu = colonies.slice(0, 10, ",");
+    var count = cfu.toString()
+    console.log(count);
   
-  var radians = degrees * Math.PI / 180;
-  var x = -1 * radius * Math.cos(radians) * wfreq;
-  var y = radius * Math.sin(radians);
-  
-  var gaugeLayout = {
-    shapes: [{
-      type: 'line',
-      x0: 0.5,
-      y0: 0.5,
-      x1: x,
-      y1: y,
-      line: {
-        color: 'black',
-        width: 3
+    var bugs = []
+      
+      for (i=0; i<cfu.length; i++){
+        bugs.push(Number(cfu[i]))
       }
-    }],
-    title: 'Belly Button washing Frequency',
-    xaxis: {visible: false, range: [-1, 1]},
-    yaxis: {visible: false, range: [-1, 1]}
-  }
-  
-  var dataGauge = [traceGauge]
-  
-  Plotly.newPlot('gauge', dataGauge, gaugeLayout)
-});
-};
 
+      console.log(bugs)
+    //Create a horizontal bar chart with a dropdown menu to display the top 
+    //10 OTUs found in that individual.
+
+    //Use otu_labels as the hovertext for the chart.
+    var otuLabel = firstDataset.map(function(samples) {
+      return samples.otu_labels;
+    });
+
+
+    var labels = otuLabel.toString().split(";");
+    var organism = labels.slice(0, 10, ";");
+    console.log(organism);
+
+    //Create trace for horizontal bar chart
+    // });      
+    var tracehbar = {
+      x: bugs,
+      y: bbmicrobes,
+      type: "bar",
+      orientation: "h",
+      hovertext: organism
+  
+    };
+          
+      //     Create the data array for the plot
+    var data = [tracehbar];
+          
+    //     Define the plot layout
+    var layout = {
+      title: "BellyBotton Biodiversity",
+      xaxis: { title: "Count" },
+      yaxis: { title: "Otu ID",
+              autotick: true},
+      
+  };
+          
+  //     Plot the chart to a div tag with id "bar"
+    Plotly.newPlot("bar", data, layout);
+
+  //     Create function to filter first subject metadata for panel    
+    function initialMetadata(data) {
+      return data.id === parseInt(option);
+    };
+
+    var clearMetadata = d3.select('#sample-metadata');
+      
+      clearMetadata.html("")
+
+    var currentMetadata = metadata.filter(initialMetadata);
+    console.log(currentMetadata)
+
+  //  Loop through firstMetadata to use object.entries to append key,value pairs to sample_metadata
+    currentMetadata.forEach((data) => {
+        Object.entries(data).forEach(([key, value]) => {
+          d3.select("#sample-metadata").append("ul").append("li").text(`${key}  :  ${value}`);
+        });
+    });
+
+          
+  //     Create a bubble chart that displays each sample.
+  
+    
+  //     Create trace for bubble chart
+    var tracebubble = {
+      x: microbes,
+      y: colonies,
+      type: "scatter",
+      mode: 'markers',
+      hovertext: labels,
+    
+      marker: {
+        size: colonies,
+        color: microbes,
+        symbol: "circle"
+    
+      },
+    
+    };
+    
+  //Create data array for bubble plot
+    var bubbledata = [tracebubble];
+    
+  //Create layout for bubble plot
+    var bubblelayout = {
+      title: 'BellyBottom Microbe Diversity',
+      showlegend: false,
+      height: 800,
+      width: 1000
+    };
+    
+  //Plot the chart to a div tag with id "bubble"
+    Plotly.newPlot('bubble', bubbledata, bubblelayout);
+
+  //     Gauge chart
+
+  //Create array for Wfreq and other
+    var wfreq = []
+    var other = []
+
+  //Loop through first metadata for wfreq
+    currentMetadata.forEach((data) => {
+
+  //Iterate through each key and value
+      Object.entries(data).forEach(([key, value]) => {
+    
+  //Use the key to determine which array to push the value to
+        if (key === "wfreq") {
+          wfreq.push(value);
+        }
+        else {
+          other.push(value);
+        };
+      });
+    });
+
+    console.log(wfreq);
+  // Use wfreq array to generate gauge plot
+
+    var traceGauge = {
+      type: 'pie',
+      rotation: 90,
+      values: [ 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180/10, 180],
+      text: ['0','1','2','3','4','5','6','7','8','9'],
+      direction: 'clockwise',
+      textinfo: 'text',
+      textposition: 'inside',
+      marker: {
+        colors: ['red','orange','yellow','tan','green','blue','purple','maroon','grey', 'cyan', 'white'],
+        labels: ['0','1','2','3','4','5','6','7','8','9'],
+        hoverinfo: 'label'
+      }
+    };
+
+
+    var degrees = 97, radius = 1.5;
+    
+    var radians = degrees * Math.PI / 180;
+    var x = 1 * radius * Math.cos(radians) * wfreq;
+    var y = radius * Math.sin(radians);
+    
+    var gaugeLayout = {
+      shapes: [{
+        type: 'line',
+        x0: 0.5,
+        y0: 0.5,
+        x1: x,
+        y1: y,
+        line: {
+          color: 'black',
+          width: 3
+        }
+      }],
+      title: 'Belly Button washing Frequency',
+      xaxis: {visible: false, range: [-1, 1]},
+      yaxis: {visible: false, range: [-1, 1]}
+    };
+    
+    var dataGauge = [traceGauge]
+    
+    Plotly.newPlot('gauge', dataGauge, gaugeLayout)
+  });
+};
 init();
 
